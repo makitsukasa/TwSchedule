d2640a51c085b481757a0ffcdb70d66b23243766
makitsukasa <allium.fistulosum.39@gmail.com>
2021-11-26 03:57:47 +0900

    AZURE_APP_CONFIG

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import traceback
 from azure.appconfiguration import AzureAppConfigurationClient, ConfigurationSetting
 from flask import Flask
 app = Flask(__name__)
@@ -8,9 +9,14 @@ def hello():
 	# return "Hello, World!"
 	try:
 		content = ""
+		connection_string = os.getenv("CUSTOMCONNSTR_AZURE_APP_CONFIG_CONNECTION_STRING")
+		app_config_client = AzureAppConfigurationClient.from_connection_string(connection_string)
 		retrieved_config_setting = app_config_client.get_configuration_setting(key='test')
 		content += "Retrieved configuration setting:<br>"
 		content += "Key: " + retrieved_config_setting.key + ", Value: " + retrieved_config_setting.value
 		return content
 	except Exception as e:
-		return "Exception:" + e
+		return "Exception:" + str(traceback.format_exc())
+
+if __name__ == "__main__":
+	app.run(debug = True)
