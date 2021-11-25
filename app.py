a00f32869acb5e29f97aca9cb74149cb7f043bfe
makitsukasa <allium.fistulosum.39@gmail.com>
2021-11-25 16:26:20 +0900

    test azure app configuration

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
+import os
+from azure.appconfiguration import AzureAppConfigurationClient, ConfigurationSetting
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	return "Hello, World!"
+	# return "Hello, World!"
+	try:
+		content = ""
+		retrieved_config_setting = app_config_client.get_configuration_setting(key='test')
+		content += "Retrieved configuration setting:<br>"
+		content += "Key: " + retrieved_config_setting.key + ", Value: " + retrieved_config_setting.value
+		return content
+	except Exception as e:
+		return "Exception:" + e
