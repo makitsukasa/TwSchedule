a002476818f58169e104c06c869d732240a8253d
makitsukasa <allium.fistulosum.39@gmail.com>
2021-11-26 23:54:12 +0900

    tweet

--- app.py
+++ app.py
@@ -1,20 +1,34 @@
 import os
+import hashlib
 import traceback
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request
+import tweepy
 
-@app.route("/")
+flask_app = Flask(__name__)
+
+KEYS = os.getenv("CUSTOMCONNSTR_TWITTER_KEYS")
+CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET = KEYS.split(";")
+AUTH = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+AUTH.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+tweepy_api = tweepy.API(AUTH)
+
+@flask_app.route("/")
 def hello():
-	# return "Hello, World!"
 	try:
-		content = ""
-		access_token = os.getenv("CUSTOMCONNSTR_TWITTER_ACCESS_TOKEN")
-		consumer_key, consumer_secret = access_token.split(";")
-		content += "key: " + consumer_key[:2] + "***" + consumer_key[-2:] + ", secret: ***"
-		return content
+		return "Hello, World!"
 
 	except Exception as e:
 		return "Exception:" + str(traceback.format_exc()), 500
 
+@flask_app.route("/tweet", methods=["POST"])
+def tweet():
+	print(request.form)
+	if not request.form.get("key") or not request.form.get("body"):
+		return "irregal access", 400
+	if request.form["key"] != hashlib.sha256(str(KEYS).encode('utf-8')).hexdigest():
+		return "irregal access", 400
+	tweepy_api.update_status(request.form["body"])
+	return "tweeted"
+
 if __name__ == "__main__":
-	app.run(debug = True)
+	flask_app.run(debug = True)
