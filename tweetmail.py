c297c5326d212b219d56082808b69ef20a312559
makitsukasa <allium.fistulosum.39@gmail.com>
2021-11-30 15:38:01 +0900

    「未読の云々」

--- tweetmail.py
+++ tweetmail.py
@@ -4,3 +4,12 @@ def update_mail_notice(body, should_update):
 	HASHTAG = "#mirua_mail #牧師録"
 	USER_ID = "tsukasa_mirua"
 	return update_notice(body, HASHTAG, USER_ID, should_update)
+
+def update_mail_notice_(unread_count, should_update):
+	HASHTAG = "#mirua_mail #牧師録"
+	USER_ID = "tsukasa_mirua"
+	if unread_count == "0":
+		body = "未読のメールはありません"
+	else:
+		body = f"@tsukasa_metam 未読のメールが{payload}件あります"
+	return update_notice(body, HASHTAG, USER_ID, should_update)
