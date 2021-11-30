5f76c2c12cc45f16a8b4ed364335699ab90eec31
makitsukasa <allium.fistulosum.39@gmail.com>
2021-12-01 05:13:22 +0900

    を受け取るよりは件数だけ受け取る方がいいかなって

--- tweetmail.py
+++ tweetmail.py
@@ -1,11 +1,6 @@
 from tweet import update_notice
 
-def update_mail_notice(body, should_update):
-	HASHTAG = "#mirua_mail #牧師録"
-	USER_ID = "tsukasa_mirua"
-	return update_notice(body, HASHTAG, USER_ID, should_update)
-
-def update_mail_notice_(unread_count, should_update):
+def update_mail_notice(unread_count, should_update):
 	HASHTAG = "#mirua_mail #牧師録"
 	USER_ID = "tsukasa_mirua"
 	if unread_count == "0":
