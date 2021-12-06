43146d6a8a53a9c7095b906ef30a91f33ebf1829
makitsukasa <allium.fistulosum.39@gmail.com>
2021-12-06 13:25:49 +0900

    Update tweet2nd.py

--- tweet2nd.py
+++ tweet2nd.py
@@ -3,8 +3,8 @@ from flask import request
 import hashlib
 import tweepy
 
-KEYS = os.getenv("CUSTOMCONNSTR_TWITTER_KEYS_2ND")
-CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET = KEYS.split(";")
+KEYS_2ND = os.getenv("CUSTOMCONNSTR_TWITTER_KEYS_2ND")
+CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET = KEYS_2ND.split(";")
 AUTH = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 AUTH.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 tweepy_api_2nd = tweepy.API(AUTH)
@@ -12,7 +12,7 @@ tweepy_api_2nd = tweepy.API(AUTH)
 def check_key():
 	if not request.form.get("key"):
 		return False
-	if request.form["key"] != hashlib.sha256(str(KEYS).encode("utf-8")).hexdigest():
+	if request.form["key"] != hashlib.sha256(str(KEYS_2ND).encode("utf-8")).hexdigest():
 		return False
 	return True
 
